860b2e17b9f800957670fba6d540addc2c65b4f7
squareleaf <tiffanydgreen@gmail.com>
2018-09-17 16:14:53 -0500

    Show all authors by looping thru array

--- src/Books.js
+++ src/Books.js
@@ -55,7 +55,9 @@ class Books extends Component {
             <div className="book">
               <div className="image_container"><img src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : ''} alt={book.volumeInfo.title} /></div>
               <div><a href={book.volumeInfo.infoLink}>{book.volumeInfo.title}</a></div>
-              <div>{book.volumeInfo.authors[0]}</div>
+              {book.volumeInfo.authors.map(author =>
+                <div>{author}</div>
+              )}
               <div className="publisher">{book.volumeInfo.publisher}</div>
             </div>
           )}
